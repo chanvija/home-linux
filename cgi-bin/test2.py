5a7bef79e0cdc55e27655b7eca4a608e1e75ab71
Chandranath Vijayaprasad <chandranath@gmail.com>
2021-07-12 11:32:58 +0530

    save changes

--- cgi-bin/test2.py
+++ cgi-bin/test2.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, render_template
 import sqlite3
 import os
 import json
@@ -7,35 +7,47 @@ app = Flask(__name__)
 
 db_file = "/usr/local/var/www/home-linux-www/html/cdj-album.db"
 
-@app.route('/test_db',methods = ['POST', 'OPTIONS'])
-def hello_world():
+@app.route('/update_file',methods = ['POST', 'OPTIONS'])
+def update_file():
     if request.method == "OPTIONS": # CORS preflight
         response = make_response()
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add('Access-Control-Allow-Headers', "*")
         response.headers.add('Access-Control-Allow-Methods', "*")
-        os.system('touch /tmp/abcd')
         return(response)
     elif request.method == 'POST':
-        new_tag = request.form['new_tag']
-        filename = request.form['filename']
-        if True:
-            conn = sqlite3.connect(db_file)
-            myCursor = conn.cursor()
-            myCursor.execute('SELECT tags FROM collection where filename = "' + filename + '"')
+        data = request.get_json()    
+        print(data.keys())    
+        new_tag = data['new_tag']
+        filter = data['filter']
+        filter_type = data['filter_type']
+        conn = sqlite3.connect(db_file)
+        myCursor = conn.cursor()
+        if filter_type == "file_single":
+            myCursor.execute('SELECT filename,tags FROM collection where filename = "' + filter + '"')
             temp = myCursor.fetchall()
-            for t in temp[0][0].split(","):
-                new_tag =  new_tag + "," + str(t) if t else new_tag
-            myCursor.execute('UPDATE collection SET tags="' + new_tag + '" WHERE filename = "' + filename + '"')
-            conn.commit()
-            myCursor.execute('SELECT tags FROM collection where filename = "' + filename + '"')
-            new_current_tags = myCursor.fetchall()
+            print(f"... old tag {temp[0][1]}")
+            if new_tag not in temp[0][1].split(","):
+                for t in temp[0][1].split(","):
+                    new_tag =  new_tag + "," + str(t) if t else new_tag
+                print(f"... new tag {new_tag}")
+                myCursor.execute('UPDATE collection SET tags="' + new_tag + '" WHERE filename = "' + filter + '"')
+                conn.commit()
+                myCursor.execute('SELECT tags FROM collection where filename = "' + filter + '"')
+                new_current_tags = myCursor.fetchall()
+                retval = jsonify(result=True, old_tag=temp[0][1],  updated_tag=new_current_tags[0][0], error=None)
+            else:
+                print(f"New tag : '{new_tag}'' already exists for this file {filter} Existing tags : {temp[0][1]}")
+                retval = jsonify(result=False, error="Tag already exists")
+                        
             conn.close()
-            retval = f"Old tag : {temp[0]}  Updated tag : {str(new_current_tags[0])}"
-            retval.headers.add("Access-Control-Allow-Origin", "*") 
-            return(retval)
         else:
-            return(filename)
+            conn.close()
+            return(jsonify(result=False, error=f"Unknown filter_type {request.form['filter_type']}"))
+                   
+        retval.headers.add("Access-Control-Allow-Origin", "*") 
+        return(retval)
+        
             
 @app.route('/get_row',methods = ['POST', 'OPTIONS'])
 def get_row():
@@ -44,37 +56,36 @@ def get_row():
         response.headers.add("Access-Control-Allow-Origin", "*")
         response.headers.add('Access-Control-Allow-Headers', "*")
         response.headers.add('Access-Control-Allow-Methods', "*")
-        os.system('touch /tmp/abcd')
         return(response)
     elif request.method == 'POST':
         data = request.get_json()        
-        tag = data['tag']
+        tag = data['tag'].lower()
         count = data['count']
         offset = data['offset']
-        if True:
-            conn = sqlite3.connect(db_file)
-            myCursor = conn.cursor()
-            if tag == "today":
-                cmd = f'SELECT filename FROM collection WHERE day = strftime("%d","now", "localtime") AND month = strftime("%m","now", "localtime") AND type LIKE "%photo%" ORDER by year,filename LIMIT {count} OFFSET {offset}'
-            else:
-                cmd = f'SELECT filename FROM collection where tags LIKE "%{tag}%" LIMIT {count}  OFFSET {offset}'
-            myCursor.execute(cmd)
-            temp = myCursor.fetchall()
-            file_list = []
-            for t in temp:
-                file_list.append(t[0])
-            conn.close()
-            print(file_list)
-            retval = jsonify(file_list)
-            retval.headers.add("Access-Control-Allow-Origin", "*") 
-            print(f".... {retval}...")
-            return(retval)
+        query_type = data['query_type']
+        conn = sqlite3.connect(db_file)
+        myCursor = conn.cursor()
+        if tag == "today":
+            cmd = f'SELECT filename,tags FROM collection WHERE day = strftime("%d","now", "localtime") AND month = strftime("%m","now", "localtime") AND type LIKE "%photo%" ORDER by year,filename LIMIT {count} OFFSET {offset}'
+            cmd = f'SELECT filename,tags FROM collection WHERE day = "10" AND month = strftime("%m","now", "localtime") AND type LIKE "%photo%" ORDER by year,filename LIMIT {count} OFFSET {offset}'
+        elif query_type == "date":
+            yy = data['year']
+            mm = data['month'].lower()
+            cmd = f'SELECT filename,tags FROM collection WHERE year = {yy} AND tags LIKE "%{mm}%" AND type LIKE "%photo%" ORDER by year,filename LIMIT {count} OFFSET {offset}'    
         else:
-            retval = filename
-            retval.headers.add("Access-Control-Allow-Origin", "*") 
-            return(retval)
-
-#app.add_url_rule('/test_db', 'hello_world', hello_world)
+            cmd = f'SELECT filename,tags FROM collection where tags LIKE "%{tag}%" AND type LIKE "%photo%" LIMIT {count}  OFFSET {offset}'
+        print(cmd)
+        myCursor.execute(cmd)
+        temp = myCursor.fetchall()
+        print(f"... rows : {temp}")
+        file_list = []
+        for t in temp:
+            file_list.append((t[0], t[1]))
+        conn.close()
+        print(f"..{file_list}..")
+        retval = jsonify(file_list)
+        retval.headers.add("Access-Control-Allow-Origin", "*") 
+        return(retval)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8081, debug=True)
--- html/js/cdj-funcs.js
+++ html/js/cdj-funcs.js
@@ -18,16 +18,79 @@ function test_show_picture(img,tags)
         width = s.w * f
         height = s.h * f    
         $("#right1").html($("<img>").attr("src", img).width(width).height(height));
-        // alert(width + " " + height)
     }); 
     // $("#right").load("2.html")  
     $("#bottom").text(decodeURIComponent(tags));
 }
 
-function changeImg(target_div_id, tag, count, offset)
+function get_scaler(x,y) {
+    cell_width = '1200'
+    cell_height = '820'
+
+    f1 = parseInt(cell_width)/x
+    f2 = parseInt(cell_height)/y
+
+    if (f1 <= f2) {
+        start = f1.toFixed(2)
+    }
+
+    _width = x * start
+    _height = y * start
+
+    m = start
+    for (let f = start;(_width <= cell_width) && (_height <= cell_height); ) {
+        _width = x * f
+        _height = y * f
+        m = f
+        f = parseFloat(parseFloat(f).toFixed(2)) + parseFloat('0.01')
+    }
+  
+    return(m)
+}
+
+function rotate_image()
 {
-    // var target_div = document.getElementById(target_div_id);
-    // target_div.src = image;
+    x = String(parent.right_frame.document.getElementById('rotate_value').value)
+
+    parent.left_frame.document.getElementById("show_image").style.transform = `rotate(${x}deg)`;
+
+    x = parseInt(x) + parseInt(90)
+    parent.right_frame.document.getElementById("rotate_value").value = x;
+}
+
+function update_tag()
+{
+    var url = "http://192.168.1.10:8081/update_file";
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+
+    filter = document.getElementById('filter').value;
+    new_tag = document.getElementById('new_tag').value
+    filter_type = document.getElementById('filter_type').value;
+
+    xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify({
+        filter: filter,
+        new_tag: new_tag,
+        filter_type: filter_type
+    }));
+    xhr.onload = function() {
+        var data = JSON.parse(this.responseText);
+        // console.log(data);
+        parent.right_frame.document.getElementById('new_tag').value = ""
+        if (data['error'] == null) {
+            parent.right_frame.document.getElementById('message').value = "Tag updated successfully"
+            parent.right_frame.document.getElementById('current_tag').value = data['updated_tag']
+        } else {
+            parent.right_frame.document.getElementById('message').value = "Tag update FAILED " + data['error']
+        }
+    };
+}
+
+function changeImg(query_type, tag, count, offset)
+{
     var url = "http://192.168.1.10:8081/get_row";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", url, true);
@@ -35,11 +98,26 @@ function changeImg(target_div_id, tag, count, offset)
     xhr.setRequestHeader('Access-Control-Allow-Headers', '*');
     xhr.setRequestHeader("Accept", "application/json");
     xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify({
-        offset: offset,
-        count: count,
-        tag: tag
-    }));
+    if (query_type == "date") {
+        mmyy = parent.left_frame.document.getElementById('datepicker').value
+        month = mmyy.split(" ")[0]
+        year = mmyy.split(" ")[1]
+        xhr.send(JSON.stringify({
+            offset: offset,
+            count: count,
+            tag: tag,
+            query_type: query_type,
+            month: month,
+            year: year
+        }));
+    } else {
+        xhr.send(JSON.stringify({
+            offset: offset,
+            count: count,
+            tag: tag,
+            query_type: query_type
+        }));
+    }
     xhr.onload = function() {
         var data = JSON.parse(this.responseText);
         console.log(data);
@@ -47,25 +125,57 @@ function changeImg(target_div_id, tag, count, offset)
         prev = "#";
         next = "#";
 
-        img = path_prefix + data[0];
+        if (data.length > 0) {
+            img = path_prefix + data[0][0];
+            $("<img/>").attr("src", img).on("load",function(){     
+                s = {w:this.width, h:this.height};   
 
-        // alert(data + " " + data.length)
+                cell_width = '1200'
+                cell_height = '820'
 
-        $("<img/>").attr("src", img).on("load",function(){     
-            s = {w:this.width, h:this.height};   
-            f = 820/s.h
-            _width = s.w * f
-            _height = s.h * f    
-            $("#show_image").html($("<img>").attr("src", img).width(_width).height(_height));
+                f1 = parseInt(cell_width)/s.w
+                f2 = parseInt(cell_height)/s.h
 
-            new_prev_offset = Number(offset) - Number("1");
-            if (new_prev_offset < 0) {
-                new_prev_offset = 0;
-            }
-            new_next_offset = Number(offset) + Number("1");
+                if (f1 <= f2) {
+                    start = f1.toFixed(2)
+                } else {
+                    start = f2.toFixed(2)
+                }
 
-            $("#prev").attr("onclick", 'changeImg("' + target_div_id + '", "' + tag + '", ' + count + ', ' + new_prev_offset + ")")
-            $("#next").attr("onclick", 'changeImg("' + target_div_id + '", "' + tag + '", ' + count + ', ' + new_next_offset + ")")
-        });
+                _width = s.w * start
+                _height = s.h * start
+
+                m = start
+                for (let f = start;(_width <= cell_width) && (_height <= cell_height); ) {
+                    _width = s.w * f
+                    _height = s.h * f
+                    m = f
+                    f = parseFloat(parseFloat(f).toFixed(2)) + parseFloat('0.01')
+                }
+
+                _width = s.w * m
+                _height = s.h * m    
+                //alert(s.w + " " + s.h + " " + m)
+                $("#show_image").html($("<img>").attr("src", img).width(_width).height(_height));
+
+                new_prev_offset = Number(offset) - Number("1");
+                if (new_prev_offset < 0) {
+                    new_prev_offset = 0;
+                }
+                new_next_offset = Number(offset) + Number("1");
+
+                $("#prev").attr("onclick", 'changeImg("' + query_type + '", "' + tag + '", ' + count + ', ' + new_prev_offset + ")")
+                $("#next").attr("onclick", 'changeImg("' + query_type + '", "' + tag + '", ' + count + ', ' + new_next_offset + ")")
+                parent.right_frame.document.getElementById('filter').value = data[0][0]
+                parent.right_frame.document.getElementById('current_tag').value = data[0][1]
+                parent.right_frame.document.getElementById('new_tag').value = ""
+                parent.right_frame.document.getElementById('message').value = ""
+                parent.right_frame.document.getElementById('rotate_value').value = "90"
+
+            });
+        } else {
+            parent.left_frame.document.getElementById('show_image').innerHTML = '<object type="text/html" data="no-images.html" ></object>';
+            $("#next").attr("onclick", 'null')
+        }
     };
 }
